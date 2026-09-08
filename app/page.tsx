a7bb92eb3eae451379a8509c802ccb65546c36c5
CHFaadit <149717781+CHFaadit@users.noreply.github.com>
2026-09-08 19:06:42 -0400

    Pushed brokerages tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import UseCases from "./components/UseCases";
 import Engine from "./components/Engine";
+import Brokerages from "./components/Brokerages";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <UseCases />
       <Engine />
+      <Brokerages />
     </main>
   );
 }
